4fe316831e1c9d9b631b8788cb4aadeb26aacca4
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-12-07 16:47:17 -0600

    secciones

---
+++ templates/maquila.php
@@ -0,0 +1,37 @@
+<?php
+    /* Template Name: Maquila */ 
+    get_header();
+?>
+
+    <section id="maquila_slider" class="template-section">
+      
+        <div class="grid">
+            <div class="col col_quienes">
+                <h1>Maquila</h1>
+                <p>Laboratorios Proservet en su división de maquila cuenta con una planta para la fabricación y un laboratorio de control de calidad.</p>
+
+                <p>Contamos una amplia experiencia en la fabricación de un gran número de productos utilizados en la línea veterinaria y personal calificado para cumplir con protocolos que garanticen la calidad final de los productos.</p>
+            </div>
+            <div class="col fondo">
+                
+            </div>
+       
+         </div>
+
+    </section>
+ <section id="maquila_planta" class="template-section">
+          
+          <h2>Maquila</h2>
+          <p>La planta de producción cuenta con capacidad <br>para producir líneas de:</p>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/areas.png" alt="Maquila">
+    
+    </section>
+     <section id="produccion" class="template-section">
+         <div class="contenedor">
+          <p>Todas las áreas de producción cuentan con tecnologías acordes con los requerimientos de la industria farmacéutica veterinaria.</p>
+          <p>Los principios de <span>ética, el respeto, la honestidad y la confiabilidad</span> garantizan que toda la información sea confidencial y exclusivamente suya. Usted podrá supervisar y auditar los procesos y tendrá seguridad en el cumplimiento de las entregas.</p>
+         </div>
+    </section>
+
+
+<?php include 'contacto.php'; ?>
